565d06fb552bc78e6ef95fbc6af9726c54722f79
Quinten van Woerkom <quintenvanwoerkom@gmail.com>
2019-11-20 11:40:25 +0100

    Added timing functionality

--- compress.cpp
+++ compress.cpp
@@ -3,6 +3,7 @@
  *  Compresses the given file using a canonicalized directed acyclic graph.
  */
 
+#include <chrono>
 #include <filesystem>
 #include <iostream>
 #include <vector>
@@ -17,8 +18,13 @@ int main(int argc, char* argv[]) {
   }
   std::cout << "Compressing " << path << "...";
 
+  auto start = std::chrono::high_resolution_clock::now();
+
   auto data = read_genome(path);
   auto compressed = shared_tree<dna>{data};
+
+  auto end = std::chrono::high_resolution_clock::now();
+  auto time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 
   auto compressed_size = compressed.size()*sizeof(decltype(compressed)::node_type);
   auto uncompressed_size = compressed.length()*sizeof(decltype(compressed)::value_type);
@@ -26,7 +32,8 @@ int main(int argc, char* argv[]) {
   std::cout << " Done!\n"
     << "Compressed size: " << compressed.size() << " nodes (" << compressed_size << " bytes)\n"
     << "Uncompressed size: " << compressed.length() << " elements (" << uncompressed_size << " bytes)\n"
-    << "Compression ratio: " << double(compressed_size)/double(uncompressed_size) << '\n';
+    << "Compression ratio: " << double(compressed_size)/double(uncompressed_size) << '\n'
+    << "Time: " << time.count() << "ms\n";
 
   return 0;
 }
