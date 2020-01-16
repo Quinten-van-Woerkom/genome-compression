676199abcf59396875c036cc0fe41b58e7cf0d21
Quinten van Woerkom <quint@Quinten-Laptop.localdomain>
2020-01-16 18:05:29 +0100

    Implemented shared_tree version optimised for balanced trees

--- include/balanced_shared_tree.h
+++ include/balanced_shared_tree.h
@@ -5,3 +5,234 @@
  *  those trees. This does mean that unbalanced trees are not supported.
  */
 
+#pragma once
+
+#include <array>
+#include <cassert>
+#include <cstdint>
+#include <filesystem>
+#include <vector>
+
+#include "robin_hood.h"
+
+#include "dna.h"
+#include "utility.h"
+
+namespace detail {
+/****************************************************************************
+ * class pointer:
+ *  Pointer type used inside the balanced shared tree. Stores not just an
+ *  index, but also two transformation bits to allow similar nodes to be
+ *  stored as a single canonical node referred to with annotated pointers
+ *  indicating the transformation applied to obtain the correct node.
+ */
+class pointer {
+public:
+  pointer(std::uint64_t index, bool mirror, bool transpose)
+  : data{index}, mirror{mirror}, transpose{transpose} {
+    // Ensures that a transformed nullptr is also null
+    const auto is_null = data == (1ull<<63)-1;
+    mirror |= is_null;
+    transpose |= is_null;
+  }
+
+  pointer(std::nullptr_t = nullptr) : pointer{(1ull<<63)-1, true, true} {}
+
+  bool operator==(const pointer& other) const noexcept { return to_ullong() == other.to_ullong(); }
+  bool operator!=(const pointer& other) const noexcept { return to_ullong() != other.to_ullong(); }
+  operator bool() const noexcept { return *this != nullptr; }
+
+  bool empty() const noexcept { return *this == nullptr; }
+  auto index() const noexcept { assert(!empty()); return data; }
+
+  bool is_mirrored() const noexcept { return mirror; }
+  bool is_transposed() const noexcept { return transpose; }
+  bool is_inverted() const noexcept { return mirror && transpose; }
+
+  auto mirrored() const noexcept { return pointer{data, !mirror, transpose}; }
+  auto transposed() const noexcept { return pointer{data, mirror, !transpose}; }
+  auto inverted() const noexcept { return pointer{data, !mirror, !transpose}; }
+
+  /**
+   * Returns the unsigned integer equivalent of the data stored in this
+   * pointer.
+   */
+  auto to_ullong() const noexcept -> unsigned long long {
+    return data | ((std::uint64_t)mirror << 62) | ((std::uint64_t)transpose << 63);
+  }
+
+private:
+  std::uint64_t data : 62;
+  bool mirror : 1;
+  bool transpose : 1;
+};
+
+/****************************************************************************
+ * class node:
+ *  Node type representing inner nodes in the tree.
+ *  Consists of two pointers to nodes or leaves one level down in the tree.
+ */
+class node {
+public:
+  node(pointer left, pointer right = nullptr) : children{left, right} {};
+
+  auto left() const noexcept { return children[0]; }
+  auto right() const noexcept { return children[1]; }
+
+  auto mirrored() const noexcept { return node{children[0].mirrored(), children[1].mirrored()}; }
+  auto transposed() const noexcept { return node{children[0].transposed(), children[1].transposed()}; }
+  auto inverted() const noexcept { return mirrored().transposed(); }
+
+  bool operator==(const node& other) const noexcept;
+  bool operator!=(const node& other) const noexcept { return !(*this == other); };
+  auto transformations(const node& other) const noexcept -> std::pair<bool, bool>;
+
+private:
+  std::array<pointer, 2> children;
+};
+}
+
+/******************************************************************************
+ * ostream operators
+ *  Ostream output formatting for pointers and nodes.
+ */
+inline auto& operator<<(std::ostream& os, const detail::pointer& pointer) {
+  if (pointer.empty()) return os << "empty";
+  else return os << "index " << pointer.index();
+}
+
+inline auto& operator<<(std::ostream& os, const detail::node& node) {
+  return os << "node<" << node.left() << ", " << node.right() << ">";
+}
+
+
+/******************************************************************************
+ * std::hash<> specialisation
+ *  Required for usage of nodes in a hashtable.
+ */
+namespace std {
+  template<> struct hash<detail::node> {
+    auto operator()(const detail::node& n) const noexcept -> std::size_t {
+      const auto left = n.left().index();
+      const auto right = n.left().index();
+      const auto transposed = n.left().is_transposed() ^ n.right().is_transposed();
+      const auto mirrored = n.left().is_mirrored() ^ n.left().is_mirrored();
+      if (left < right) return detail::hash(1, transposed, mirrored, left, right);
+      else return detail::hash(0, transposed, mirrored, right, left);
+    }
+  };
+}
+
+/******************************************************************************
+ * class balanced_shared_tree:
+ *  Shared binary tree class that exploits structural properties of balanced
+ *  trees to store its directed acyclic graph representation more effectively.
+ */
+class balanced_shared_tree {
+public:
+  using pointer = detail::pointer;
+  using node = detail::node;
+
+  balanced_shared_tree(std::filesystem::path path);
+
+  auto depth() const { return nodes.size(); }
+  auto width() const { return children(nodes.size()-1, root); }
+
+  auto children(std::size_t layer, pointer pointer) const -> std::size_t;
+  auto node_count() const -> std::size_t;
+  auto node_count(std::size_t layer) const { return nodes[layer].size(); }
+  auto leaf_count() const { return leaves.size(); }
+
+  auto access_leaf(pointer pointer) const -> dna;
+  auto access_node(std::size_t layer, pointer pointer) const -> node;
+  auto operator[](std::uint64_t index) const -> dna;
+
+  void add_layer() { nodes.emplace_back(); }
+  void emplace_node(std::size_t layer, node node);
+  void emplace_leaf(dna leaf);
+
+  void histogram(std::filesystem::path) {};
+
+  struct iterator {
+    using pointer = balanced_shared_tree::pointer;
+    using node = balanced_shared_tree::node;
+
+    struct status {
+      status(std::size_t layer, pointer current)
+      : layer{layer}, current{current} {}
+
+      std::size_t layer;  // Leaf node is denoted by max std::size_t value
+      iterator::pointer current;
+    };
+
+    iterator(balanced_shared_tree& nodes, std::size_t layer, pointer root);
+
+    auto operator*() const noexcept -> dna;
+    auto operator++() -> iterator&;
+    auto operator!=(const iterator& other) const { return !stack.empty(); }
+    void next_leaf();
+
+    balanced_shared_tree& parent;
+    std::vector<status> stack;
+  };
+
+  using const_iterator = iterator;
+
+  auto begin() { return iterator{*this, nodes.size()-1, root}; }
+  auto end() { return iterator{*this, 0, nullptr}; }
+
+private:
+  std::vector<std::vector<node>> nodes;
+  std::vector<dna> leaves;
+  pointer root;
+};
+
+
+/******************************************************************************
+ * class tree_constructor:
+ *  Helper class in construction of a balanced shared tree.
+ *  Contains the maps used to link nodes to pointers or leaves.
+ */
+class tree_constructor {
+public:
+  using pointer = balanced_shared_tree::pointer;
+  using node = balanced_shared_tree::node;
+  
+  // template<typename T>
+  // using hash_map = robin_hood::unordered_flat_map<T, std::uint64_t>;
+  template<typename T>
+  using hash_map = robin_hood::unordered_node_map<T, std::uint64_t>;
+
+  tree_constructor(balanced_shared_tree& parent);
+
+  auto root() const -> pointer { return roots.front(); }
+
+  void emplace_node(pointer left, pointer right = nullptr);
+  void emplace_leaf(const dna& leaf);
+  void reduce_once();
+
+  template<typename Iterable>
+  void reduce(Iterable&& segment);
+  void reduce();
+
+private:
+  balanced_shared_tree& parent;
+  std::vector<hash_map<node>> nodes;
+  hash_map<dna> leaves;
+  std::vector<pointer> current_layer;
+  std::vector<pointer> next_layer;
+  std::vector<pointer> roots;
+  std::size_t layer = 0;
+};
+
+/**
+ * Reduces the layer, saving any encountered nodes and leaves in the shared
+ * tree according to canonical representation.
+ */
+template<typename Iterable>
+void tree_constructor::reduce(Iterable&& segment) {
+  layer = 0;
+  for (auto leaf : segment) emplace_leaf(leaf);
+  while (current_layer.size() > 1) reduce_once();
+  roots.emplace_back(current_layer.front());
+}
--- src/balanced_shared_tree.cpp
+++ src/balanced_shared_tree.cpp
@@ -4,3 +4,303 @@
  *  Specialised for balanced trees, to improve compression speed and ratio for
  *  those trees. This does mean that unbalanced trees are not supported.
  */
+
+#include <limits>
+
+#include "balanced_shared_tree.h"
+
+#include "fasta_reader.h"
+
+/****************************************************************************
+ * class node:
+ *  Node type representing inner nodes in the tree.
+ *  Consists of two pointers to nodes or leaves one level down in the tree.
+ */
+/**
+ * Two nodes compare equal when they can be transformed into one another
+ * through any possible combination of similarity transforms.
+ */
+bool detail::node::operator==(const node& other) const noexcept {
+  return children == other.children
+  || mirrored().children == other.children
+  || transposed().children == other.children
+  || inverted().children == other.children;
+}
+
+/**
+ * Determines the transformations required to obtain other from the current
+ * node. First boolean corresponds to mirroring, second to transposition.
+ * Precondition: other must be similar to this.
+ */
+auto detail::node::transformations(const node& other) const noexcept -> std::pair<bool, bool> {
+  if (children == other.children) return {false, false};
+  if (mirrored().children == other.children) return {true, false};
+  if (transposed().children == other.children) return {false, true};
+  else return {true, true};
+}
+
+
+/******************************************************************************
+ * class balanced_shared_tree:
+ *  Shared binary tree class that exploits structural properties of balanced
+ *  trees to store its directed acyclic graph representation more effectively.
+ */
+/**
+ * Constructs a balanced_shared_tree from a FASTA formatted file.
+ */
+balanced_shared_tree::balanced_shared_tree(std::filesystem::path path) {
+  constexpr auto segment_size = (1u<<29);
+  auto data = fasta_reader{path};
+  auto constructor = tree_constructor{*this};
+  auto subroots = std::vector<pointer>{};
+
+  for (auto segment : chunks(data, segment_size))
+    constructor.reduce(segment);
+  constructor.reduce();
+  root = constructor.root();
+}
+
+/**
+ * Returns the total number of nodes in the tree.
+ * Leafs are excluded.
+ */
+auto balanced_shared_tree::node_count() const -> std::size_t {
+  auto sum = 0ull;
+  for (const auto& layer : nodes) sum += layer.size();
+  return sum;
+}
+
+/**
+ * Accesses the leaf pointed to by <pointer>.
+ * Applies the correct transforms to the canonical leaf to obtain the result.
+ */
+auto balanced_shared_tree::access_leaf(pointer pointer) const -> dna {
+  auto result = leaves[pointer.index()];
+  if (pointer.is_mirrored()) result = result.mirrored();
+  if (pointer.is_transposed()) result = result.transposed();
+  return result;
+}
+
+/**
+ * Accesses the node in layer <layer> pointed to by <pointer>.
+ * Returned by value since since the nodes must be immutable anyway.
+ */
+auto balanced_shared_tree::access_node(std::size_t layer, pointer pointer) const -> node {
+  return nodes[layer][pointer.index()];
+}
+
+/**
+ * Returns the number of children contained in the subtree referenced by
+ * <pointer> in <layer>.
+ */
+auto balanced_shared_tree::children(std::size_t layer, pointer pointer) const -> std::size_t {
+  if (pointer == nullptr) return 0;
+  const auto node = access_node(layer, pointer);
+  const auto left = node.left();
+  const auto right = node.right();
+  if (layer == 0) return !left.empty() + !right.empty();
+  else return children(layer-1, left) + children(layer-1, right);
+}
+
+auto balanced_shared_tree::operator[](std::uint64_t index) const -> dna {
+  auto current = root;
+  auto layer = nodes.size() - 1;
+
+  auto combine = [](auto next, auto current) {
+    const auto mirror = next.is_mirrored() != current.is_mirrored();
+    const auto transpose = next.is_transposed() != current.is_transposed();
+    return pointer{next.index(), mirror, transpose};
+  };
+
+  auto descend = [&](auto left, auto right) {
+    const auto size = children(--layer, left);
+    if (index < size) return combine(left, current);
+    index -= size;
+    return combine(right, current);
+  };
+
+  while (layer > 0) {
+    const auto& node = access_node(layer, current);
+    const auto left = node.left();
+    const auto right = node.right();
+
+    if (current.is_mirrored()) current = descend(right, left);
+    else current = descend(left, right);
+  }
+
+  const auto& node = access_node(0, current);
+  const auto left = node.left();
+  const auto right = node.right();
+
+  auto descend_leaf = [&](auto left, auto right) {
+    const auto size = (bool)left;
+    if (index < size) return combine(left, current);
+    index -= size;
+    return combine(right, current);
+  };
+
+  if (current.is_mirrored()) current = descend_leaf(right, left);
+  else current = descend_leaf(left, right);
+  return access_leaf(current);
+}
+
+/**
+ * Adds a node to the specified layer.
+ * Precondition: no similar nodes are already present in this layer. 
+ */
+void balanced_shared_tree::emplace_node(std::size_t layer, node node) {
+  nodes[layer].emplace_back(node);
+}
+
+/**
+ * Adds a leaf to the leaf layer.
+ * Precondition: the leaf is in its canonical shape and not yet present.
+ */
+void balanced_shared_tree::emplace_leaf(dna leaf) {
+  leaves.emplace_back(leaf);
+}
+
+
+/******************************************************************************
+ * class balanced_shared_tree::iterator:
+ *  Iterator over the tree.
+ */
+balanced_shared_tree::iterator::iterator(balanced_shared_tree& parent, std::size_t layer, detail::pointer root) : parent{parent} {
+  if (root) {
+    stack.emplace_back(layer, root);
+    next_leaf();
+  }
+}
+
+/**
+ * Returns the DNA strand stored in the current leaf.
+ * Mirrors and transposes it, if necessary.
+ * Precondition: current stack top points to a leaf.
+ */
+auto balanced_shared_tree::iterator::operator*() const noexcept -> dna {
+  auto top = stack.back().current;
+  return parent.access_leaf(top);
+}
+
+/**
+ *  Advances the iterator to the first leaf on the right of the current leaf.
+ *  To achieve this, pops the last-found leaf off the stack and recurses on the
+ *  remaining stack members until a leaf is found.
+ */
+auto balanced_shared_tree::iterator::operator++() -> iterator& {
+  stack.pop_back();
+  next_leaf();
+  return *this;
+}
+
+/**
+ *  Iterates over the tree nodes until a leaf is found, or the stack is empty.
+ *  For each encountered node, determines if it is a leaf. If not, its children
+ *  are added to the stack and we apply the same procedure to them, starting
+ *  with the left child.
+ */
+void balanced_shared_tree::iterator::next_leaf() {
+  while (!stack.empty()) {
+    auto status = stack.back();
+    // Leaf level is indicated as the maximum std::size_t value
+    if (status.layer == std::numeric_limits<std::size_t>::max()) return;
+
+    auto top = status.current;
+    auto node = parent.access_node(status.layer, status.current);
+    stack.pop_back();
+
+    // Apply mirroring and transposition, if necessary, and save the resulting
+    // state on the stack.
+    auto stack_push = [&](auto next) {
+        auto mirror = next.is_mirrored() != top.is_mirrored();
+        auto transpose = next.is_transposed() != top.is_transposed();
+        auto updated_pointer = pointer{next.index(), mirror, transpose};
+        stack.emplace_back(status.layer - 1, updated_pointer);
+    };
+
+    if (top.is_mirrored()) {
+      if (node.left()) stack_push(node.left());
+      if (node.right()) stack_push(node.right());
+    } else {
+      if (node.right()) stack_push(node.right());
+      if (node.left()) stack_push(node.left());
+    }
+  }
+}
+
+/******************************************************************************
+ * class tree_constructor:
+ *  Helper class in construction of a balanced shared tree.
+ *  Contains the maps used to link nodes to pointers or leaves.
+ */
+tree_constructor::tree_constructor(balanced_shared_tree& parent)
+: parent{parent} {}
+
+/**
+ * Constructs and emplaces a node inside the tree during its construction.
+ * A pointer to the node is stored in <next_layer>.
+ */
+void tree_constructor::emplace_node(pointer left, pointer right) {
+  auto created_node = node{left, right};
+  auto insertion = nodes[layer].emplace(created_node, parent.node_count(layer));
+  auto canonical_node = (*insertion.first).first;
+  auto index = (*insertion.first).second;
+
+  if (insertion.second) {
+    parent.emplace_node(layer, created_node);
+    next_layer.emplace_back(index, false, false);
+  } else {
+    auto transform = created_node.transformations(canonical_node);
+    next_layer.emplace_back(index, transform.first, transform.second);
+  }
+}
+
+/**
+ * Constructs and emplaces a leaf inside the tree during its construction.
+ * A pointer to the leaf is stored in <current_layer>, in preparation for
+ * further reduction.
+ */
+void tree_constructor::emplace_leaf(const dna& leaf) {
+  auto [canonical_leaf, mirror, transpose] = leaf.canonical();
+  auto insertion = leaves.emplace(canonical_leaf, parent.leaf_count());
+  auto index = (*insertion.first).second;
+
+  if (insertion.second) parent.emplace_leaf(canonical_leaf);
+  current_layer.emplace_back(index, mirror, transpose);
+}
+
+/**
+ * Reduces the current layer by constructing nodes out of the pointers it
+ * contains, and emplacing those nodes in the correct layers and maps, if
+ * necessary.
+ */
+void tree_constructor::reduce_once() {
+  next_layer.reserve(current_layer.size()/2 + current_layer.size()%2);
+  if (parent.depth() <= layer) {
+    parent.add_layer();
+    nodes.emplace_back();
+  }
+
+  foreach_pair(current_layer,
+    [&](const auto& left, const auto& right) { emplace_node(left, right); },
+    [&](const auto& last) { emplace_node(last); }
+  );
+
+  current_layer = std::move(next_layer);
+  next_layer.clear();
+  ++layer;
+}
+
+
+/**
+ * Combines the constructed subtrees into a single tree by further reduction.
+ * Precondition: <layer> is valued at the correct index for the subroots.
+ */
+void tree_constructor::reduce() {
+  current_layer = roots;
+  roots.clear();
+  while (current_layer.size() > 1) {
+    reduce_once();
+  } 
+  roots.emplace_back(current_layer.front());
+}
