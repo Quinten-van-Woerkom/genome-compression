158f1ecb8d115a9ad0908623b779a0422924f7bb
Quinten van Woerkom <quint@Quinten-Laptop.localdomain>
2020-01-17 14:39:35 +0100

    Reduced compress output to non-debug information

--- compress.cpp
+++ compress.cpp
@@ -32,29 +32,21 @@ int main(int argc, char* argv[]) {
 
   std::cout << "Done!\n" << std::flush;
 
-  compressed.print_unique(std::cout);
-
-  std::cout << "Calculating node count\n" << std::flush;
   auto compressed_size = compressed.node_count();
-  std::cout << "Calculating tree width\n" << std::flush;
   auto compressed_width = compressed.width();
-  std::cout << "Calculating file size\n" << std::flush;
   auto file_size = std::filesystem::file_size(path);
   if (!histogram.empty()) {
-    std::cout << "Calculating histogram\n" << std::flush;
     compressed.histogram(histogram);
+    std::cout << "Stored node reference histogram at " << histogram << '\n';
   }
-  std::cout << "Printing results:\n" << std::flush;
 
   std::cout
     << "Number of data units: " << compressed_width << '\n'
     << "Number of base pairs: " << compressed_width*dna::size() << '\n'
     << "File size: " << file_size << " bytes\n"
     << "Compressed size: " << compressed_size*sizeof(balanced_shared_tree::node) << " bytes (" << compressed_size << " nodes)\n"
-    << "Compression ratio: " << double(compressed_size*sizeof(balanced_shared_tree::node))/double(file_size) << '\n';
-  if (!histogram.empty())
-    std::cout << "Stored node reference histogram at " << histogram << '\n';
-  std::cout << "Time: " << time.count() << "ms\n";
+    << "Compression ratio: " << double(compressed_size*sizeof(balanced_shared_tree::node))/double(file_size) << '\n'
+    << "Time: " << time.count() << "ms\n";
 
   return 0;
 }
